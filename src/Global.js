40c3580b4c144e954499c4b7b082c6b049054bdb
luismi937 <luismicanizares02@gmail.com>
2025-10-31 09:10:04 +0100

    up

---
+++ src/Global.js
@@ -0,0 +1,7 @@
+var Global={
+    urlCoches:"https://apicochespaco.azurewebsites.net/",
+    urlCustomer:"https://services.odata.org/V4/Northwind/Northwind.svc/",
+    urlEmpleados:"https://apiempleadosfullstack.azurewebsites.net/"
+
+}
+export default Global;
