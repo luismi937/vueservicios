915659b2df014aef4dc72c49491728690e1cf0c3
luismi937 <luismicanizares02@gmail.com>
2025-11-04 09:28:04 +0100

    up

--- src/Router.js
+++ src/Router.js
@@ -4,13 +4,15 @@ import CochesComponent from "./components/CochesComponent.vue";
 import EmpleadosDetailsComponent from "./components/EmpleadosDetailsComponent.vue";
 import CustomersComponent from "./components/CustomersComponent.vue";
 import EmpleadosOficios from "./components/EmpleadosOficios.vue";
+import DeleteComponent from "./components/DeleteComponent.vue";
 
 const myRoutes=[
     {path:"/",component:HomeComponent},
     {path:"/coches",component:CochesComponent},
     {path:"/empleados",component:EmpleadosDetailsComponent},
     {path:"/customers",component:CustomersComponent},
-    {path: "/EmpleadosOficios/:oficio", component: EmpleadosOficios}
+    {path: "/EmpleadosOficios/:oficio", component: EmpleadosOficios},
+    {path: "/delete", component: DeleteComponent}
 
 ]
 
